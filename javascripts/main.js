d6384c9beb466666ba2ca19a48684baa27ddb186
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-09-27 20:06:44 -0500

    added veggie maker

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,13 @@
 import bread from './components/bread.js';
 import meat from './components/meat.js';
 import cheese from './components/cheese.js';
+import veggie from './components/veggie.js';
 
 const init = () => {
     bread.printBreadOptions();
     meat.printMeatOptions();
     cheese.printCheeseOptions();
+    veggie.printVeggieOptions();
 };
 
 init()
